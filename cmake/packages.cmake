c558093a72fe5330fcc7285ff0badfb2a66b0c6c
Chia-Hua Chang <CHChang810716@gmail.com>
2019-07-05 04:21:04 +0800

    [WIP] logger

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -4,4 +4,19 @@ if(BUILD_TEST)
     hunter_add_package(GTest)
     find_package(GTest CONFIG REQUIRED)
 endif()
+hunter_add_package(Beast)
+find_package(Beast CONFIG REQUIRED)
+
+hunter_add_package(range-v3)
+find_package(range-v3 CONFIG REQUIRED)
+
+hunter_add_package(spdlog)
+find_package(spdlog CONFIG REQUIRED)
+
+hunter_add_package(fmt)
+find_package(fmt CONFIG REQUIRED)
+
+hunter_add_package(nlohmann_json)
+find_package(nlohmann_json CONFIG REQUIRED)
+
 find_package(Threads REQUIRED)
