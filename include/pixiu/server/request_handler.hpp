a7a1ea13e8e8d4d44ebcd7660df979286584d637
Chia-Hua Chang <CHChang810716@gmail.com>
2019-07-19 08:39:47 +0800

    update request handler

--- include/pixiu/server/request_handler.hpp
+++ include/pixiu/server/request_handler.hpp
@@ -10,56 +10,40 @@ namespace __beast   = boost::beast        ;
 namespace __asio    = boost::asio         ;
 
 struct request_handler {
-  using request_body = __http::request<__http::string_body>;
-  // using sender = std::function<void()
+  using request_body        = __http::request<__http::string_body>;
+  using response            = __http::response<__http::string_body>;
+  using empty_response      = __http::response<__http::empty_body>;
+  using handler             = std::function<response(request_body)>;
+  using server_err_handler  = std::function<response(request_body, std::string)>;
+  using rep_nobody_handler  = std::function<empty_response(request_body)>;
+  template<class Handler>
+  using request_router      = std::map<std::string, Handler>;
   static auto& logger() {
     return pixiu::logger::get("request_handler");
+  }
+  request_handler() {
+    on_err_unknown_method_ = [this](request_body req) -> response {
+      return bad_request(req, "Unknown HTTP method");
+    };
+    on_err_illegal_target_ = [this](request_body req) -> response {
+      return bad_request(req, "Illegal request target");
+    };
+    on_err_resource_not_found_ = [this](request_body req) -> response {
+      return not_found(req);
+    };
+    on_err_server_error_ = [this](request_body req, std::string what) -> response {
+      return server_error(req, what);
+    };
   }
   template<class Func>
   void operator()(
     request_body  req, 
     Func&&        send
   ) const {
-    // Returns a bad request response
-    auto const bad_request =
-    [&req](boost::beast::string_view why) {
-      __http::response<__http::string_body> res{
-        __http::status::bad_request, req.version()
-      };
-      res.set(__http::field::server, BOOST_BEAST_VERSION_STRING);
-      res.set(__http::field::content_type, "text/html");
-      res.keep_alive(req.keep_alive());
-      res.body() = why.to_string();
-      res.prepare_payload();
-      return res;
-    };
-
-    // Returns a not found response
-    auto const not_found =
-    [&req](boost::beast::string_view target) {
-        __http::response<__http::string_body> res{
-          __http::status::not_found, req.version()
-        };
-        res.set(__http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(__http::field::content_type, "text/html");
-        res.keep_alive(req.keep_alive());
-        res.body() = "The resource '" + target.to_string() + "' was not found.";
-        res.prepare_payload();
-        return res;
-    };
 
     // Returns a server error response
     auto const server_error =
     [&req](boost::beast::string_view what) {
-        __http::response<__http::string_body> res{
-          __http::status::internal_server_error, req.version()
-        };
-        res.set(__http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(__http::field::content_type, "text/html");
-        res.keep_alive(req.keep_alive());
-        res.body() = "An error occurred: '" + what.to_string() + "'";
-        res.prepare_payload();
-        return res;
     };
 
     // Make sure we can handle the method
@@ -73,23 +57,6 @@ struct request_handler {
         req.target().find("..") != boost::beast::string_view::npos)
         return send(bad_request("Illegal request-target"));
 
-    // // Build the path to the requested file
-    // std::string path = path_cat(doc_root, req.target());
-    // if(req.target().back() == '/')
-    //     path.append("index.html");
-
-    // // Attempt to open the file
-    // boost::beast::error_code ec;
-    // http::file_body::value_type body;
-    // body.open(path.c_str(), boost::beast::file_mode::scan, ec);
-
-    // // Handle the case where the file doesn't exist
-    // if(ec == boost::system::errc::no_such_file_or_directory)
-    //     return send(not_found(req.target()));
-
-    // // Handle an unknown error
-    // if(ec)
-    //     return send(server_error(ec.message()));
     __http::string_body::value_type body;
     body = "server works !!!";
 
@@ -120,6 +87,51 @@ struct request_handler {
     res.keep_alive(req.keep_alive());
     return send(std::move(res));
   }
+private:
+  response bad_request(request_body req, std::string why) {
+    __http::response<__http::string_body> res{
+      __http::status::bad_request, req.version()
+    };
+    res.set(__http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(__http::field::content_type, "text/html");
+    res.keep_alive(req.keep_alive());
+    res.body() = why;
+    res.prepare_payload();
+    return res;
+  }
+  response not_found(request_body req) {
+    __http::response<__http::string_body> res{
+      __http::status::not_found, req.version()
+    };
+    res.set(__http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(__http::field::content_type, "text/html");
+    res.keep_alive(req.keep_alive());
+    res.body() = "The resource '" + req.target().to_string() + "' was not found.";
+    res.prepare_payload();
+    return res;
+  }
+  response server_error(request_body req, std::string what) {
+    __http::response<__http::string_body> res{
+      __http::status::internal_server_error, req.version()
+    };
+    res.set(__http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(__http::field::content_type, "text/html");
+    res.keep_alive(req.keep_alive());
+    res.body() = "An error occurred: '" + what + "'";
+    res.prepare_payload();
+    return res;
+
+  }
+  handler               on_err_unknown_method_      ;
+  handler               on_err_illegal_target_      ;
+  handler               on_err_resource_not_found_  ;
+  server_err_handler    on_err_server_error_        ;
+  request_router<
+    rep_nobody_handler
+  >                     on_head_request_        ;
+  request_router<
+    handler
+  >                     on_get_request_         ;
 
 };
 
