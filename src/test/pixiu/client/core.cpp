75dbf2a6f5c00a5a7fce3bcf76462e2190a3dea6
Chia-Hua Chang <CHChang810716@gmail.com>
2019-08-14 21:36:42 +0800

    client test workable

--- src/test/pixiu/client/core.cpp
+++ src/test/pixiu/client/core.cpp
@@ -1,6 +1,8 @@
 #include <pixiu/client/core.hpp>
 #include <gtest/gtest.h>
 #include <boost/beast/core/buffers_to_string.hpp>
+#include <pixiu/path.hpp>
+#include <iterator>
 
 class core_test 
 : public ::testing::Test 
@@ -9,16 +11,7 @@ protected:
   static void SetUpTestCase() {
     nlohmann::json data;
     auto& loggers = data["loggers"];
-    loggers["plain_http"] = {
-      {"level", "debug"}
-    };
-    loggers["http_base"] = {
-      {"level", "debug"}
-    };
-    loggers["core"] = {
-      {"level", "debug"}
-    };
-    loggers["request_handler"] = {
+    loggers["client"] = {
       {"level", "debug"}
     };
     pixiu::logger::config(data);
@@ -37,16 +30,24 @@ protected:
 //   ioc.run_for(std::chrono::seconds(2));
 // }
 TEST_F(core_test, async_read_test) {
+  using namespace boost::beast;
+  std::string actual;
   boost::asio::io_context ioc;
   auto core = pixiu::client::make_core(ioc);
   core->async_read(
-    "www.google.com", "80", 
-    11, {}, 
-    [](boost::system::error_code ec, pixiu::client::responses reps){
-      for(auto& rep : reps) {
-        std::cout << boost::beast::buffers_to_string(rep.body().data()) << std::endl;
-      }
+    "www.posttestserver.com", "80", 
+    11, {
+      {"/", http::verb::get, {} }
+    }, 
+    [&actual](boost::system::error_code ec, pixiu::client::responses reps){
+      actual = buffers_to_string(reps.at(0).body().data());
+      // for(auto& rep : reps) {
+      //   std::cout << boost::beast::buffers_to_string(rep.body().data()) << std::endl;
+      // }
     }
   );
   ioc.run();
+  std::ifstream fin((pixiu::path.test_data() / "client_test_site.txt").string());
+  std::string expect((std::istreambuf_iterator<char>(fin)), std::istreambuf_iterator<char>());
+  EXPECT_EQ(expect, actual);
 }
