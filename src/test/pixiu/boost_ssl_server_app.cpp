7f583b8037966889ee31e0651a91dcea98267271
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-07 22:17:18 +0800

    update test tsl server code

--- src/test/pixiu/boost_ssl_server_app.cpp
+++ src/test/pixiu/boost_ssl_server_app.cpp
@@ -29,6 +29,7 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include "load_server_certificate.hpp"
 
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
@@ -64,53 +65,6 @@ auto async_write(T&&... args) {
 
 }
 
-inline void load_server_certificate(boost::asio::ssl::context& ctx) {
-  /*
-      The certificate was generated from CMD.EXE on Windows 10 using:
-
-      winpty openssl dhparam -out dh.pem 2048
-      winpty openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 10000 -out cert.pem -subj "//C=US\ST=CA\L=Los Angeles\O=Beast\CN=www.example.com"
-  */
-
-  std::string const cert =
-    "-----BEGIN CERTIFICATE-----\n"
-    "-----END CERTIFICATE-----\n"
-  ;
-
-  std::string const key =
-    "-----BEGIN PRIVATE KEY-----\n"
-    "-----END PRIVATE KEY-----\n"
-  ;
-
-  std::string const dh = 
-    "-----BEGIN DH PARAMETERS-----\n"
-    "-----END DH PARAMETERS-----\n"
-  ;
-
-  ctx.set_password_callback(
-    [](std::size_t,
-        boost::asio::ssl::context_base::password_purpose)
-    {
-        return "test";
-    });
-
-  ctx.set_options(
-      boost::asio::ssl::context::default_workarounds |
-      boost::asio::ssl::context::no_sslv2 |
-      boost::asio::ssl::context::single_dh_use);
-  // ctx.set_options(
-  //     boost::asio::ssl::context::default_workarounds |
-  //     boost::asio::ssl::context::no_sslv2);
-
-  ctx.use_certificate_chain(
-    boost::asio::buffer(cert.data(), cert.size()));
-
-  ctx.use_private_key(
-    boost::asio::buffer(key.data(), key.size()),
-    boost::asio::ssl::context::file_format::pem);
-  ctx.use_tmp_dh(
-    boost::asio::buffer(dh.data(), dh.size()));
-}
 // Return a reasonable mime type based on the extension of a file.
 boost::beast::string_view
 mime_type(boost::beast::string_view path)
--- src/test/pixiu/ssl_server_app.cpp
+++ src/test/pixiu/ssl_server_app.cpp
@@ -1,52 +1,9 @@
 #include <pixiu/server.hpp>
 #include <pixiu/response.hpp>
+#include "load_server_certificate.hpp"
 using namespace boost::beast;
 namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
 // https://blog.miniasp.com/post/2019/02/25/Creating-Self-signed-Certificate-using-OpenSSL
-inline void load_server_certificate(boost::asio::ssl::context& ctx) {
-  /*
-      The certificate was generated from CMD.EXE on Windows 10 using:
-
-      winpty openssl dhparam -out dh.pem 2048
-      winpty openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 10000 -out cert.pem -subj "//C=US\ST=CA\L=Los Angeles\O=Beast\CN=www.example.com"
-  */
-
-  std::string const cert =
-    "-----BEGIN CERTIFICATE-----\n"
-    "-----END CERTIFICATE-----\n"
-  ;
-
-  std::string const key =
-    "-----BEGIN PRIVATE KEY-----\n"
-    "-----END PRIVATE KEY-----\n"
-  ;
-
-  std::string const dh = 
-    "-----BEGIN DH PARAMETERS-----\n"
-    "-----END DH PARAMETERS-----\n"
-  ;
-
-  ctx.set_password_callback(
-    [](std::size_t,
-        boost::asio::ssl::context_base::password_purpose)
-    {
-        return "test";
-    });
-
-  ctx.set_options(
-      boost::asio::ssl::context::default_workarounds |
-      boost::asio::ssl::context::no_sslv2 |
-      boost::asio::ssl::context::single_dh_use);
-
-  ctx.use_certificate_chain(
-    boost::asio::buffer(cert.data(), cert.size()));
-
-  ctx.use_private_key(
-    boost::asio::buffer(key.data(), key.size()),
-    boost::asio::ssl::context::file_format::pem);
-  ctx.use_tmp_dh(
-    boost::asio::buffer(dh.data(), dh.size()));
-}
 void config_logger() {
     nlohmann::json data;
     auto& loggers = data["loggers"];
